ce9452a21588e2483f267f1f7cce0284ff5e2451
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2023-04-27 01:14:44 +0300

    Update user.py

--- user/user.py
+++ user/user.py
@@ -1,5 +1,6 @@
 import socket
 import hashlib
+from tqdm import tqdm
 
 # Set up listening socket
 user_addr = ('10.9.0.6', 9000)
@@ -7,23 +8,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(user_addr)
 s.listen()
 
+
 # Wait for connection from sender
 print("User is listening...")
 conn, addr = s.accept()
 print(f"Connection established with {addr}.")
 
-# Receive file data
-filedata = b''
-while True:
-    data = conn.recv(1024)
-    print(data)
-    print("recived data in user")
-    if not data:
-        break
-    filedata += data
 
-with open("file_received.txt", "wb") as f:
-    f.write(filedata)
-    
+data = conn.recv(1024)
+
+filedata = b''
+filehash = b''
+
+print(data)
+filedata, filehash  = data.split(b'__EOF__')
+
+md5_hash = hashlib.md5(filedata).hexdigest()
+print(filedata)
+print(filehash)
+
+# Compare computed hash with given hash
+if filehash.decode() == md5_hash:
+    print("File integrity verified. Hashes match.")
+else:
+    print("File integrity verification failed. Hashes do not match.")
+
 # Close the connection
 s.close()
