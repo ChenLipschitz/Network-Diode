b3bd8f7046df8c73bd6dca7a3048d95cd2ffbcf5
ChenLipschitz <93311801+ChenLipschitz@users.noreply.github.com>
2023-04-27 01:13:20 +0300

    Update sender.py

--- sender/sender.py
+++ sender/sender.py
@@ -1,6 +1,7 @@
 import socket
 import hashlib
 
+
 filename = 'file.txt'
 host = '10.9.0.3'
 port = 6000
@@ -8,10 +9,12 @@ port = 6000
 with open(filename, 'rb') as f:
     data = f.read()
 
+
 md5_hash = hashlib.md5(data).hexdigest()
+data += b'__EOF__'
 print(f"MD5 hash of {filename}: {md5_hash}")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    s.sendall(md5_hash.encode())
     s.sendall(data)
+    s.sendall(md5_hash.encode())
